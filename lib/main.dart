205fc2941776dcc98d31382e54d279d11dca1ab1
ecarlson1 <erikmathewcarlson@gmail.com>
2022-04-27 16:06:18 -0500

    Added camera and gallery functionality to buttons with image_picker

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +12,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  void _openCamera() async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? image = await _picker.pickImage(source: ImageSource.camera);
+  }
+
+  void _openGallery() async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,36 +29,39 @@ class MyApp extends StatelessWidget {
             title: Text('Boggle Solver'),
           ),
           body: Center(
-              child: Column(children: <Widget>[
-            Container(
-              child: Icon(Icons.add_a_photo,
-                  size: 100, color: Color.fromARGB(255, 71, 149, 236)),
-            ),
-            Container(
-              margin: EdgeInsets.all(25),
-              child: FlatButton(
-                child: Text(
-                  'Take Picture',
-                  style: TextStyle(fontSize: 15.0),
-                ),
-                color: Color.fromARGB(255, 71, 149, 236),
-                textColor: Color.fromARGB(255, 250, 250, 250),
-                onPressed: () {},
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.all(25),
-              child: FlatButton(
-                child: Text(
-                  'Select From Camera Roll',
-                  style: TextStyle(fontSize: 15.0),
-                ),
-                color: Color.fromARGB(255, 71, 149, 236),
-                textColor: Color.fromARGB(255, 250, 250, 250),
-                onPressed: () {},
-              ),
-            ),
-          ]))),
+              child: Column(
+                children: <Widget>[
+                  const Icon(
+                    Icons.add_a_photo,
+                    size: 100, 
+                    color: Color.fromARGB(255, 71, 149, 236)
+                  ),
+                  
+                  Container(
+                    margin: const EdgeInsets.all(25),
+                    child: FlatButton(
+                      onPressed: _openCamera,
+                      child: const Text(
+                        'Take Picture',
+                        style: TextStyle(fontSize: 15.0),
+                      ),
+                      color: const Color.fromARGB(255, 71, 149, 236),
+                      textColor: const Color.fromARGB(255, 250, 250, 250),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(25),
+                    child: FlatButton(
+                      child: const Text(
+                        'Select From Camera Roll',
+                        style: TextStyle(fontSize: 15.0),
+                      ),
+                      color: const Color.fromARGB(255, 71, 149, 236),
+                      textColor: const Color.fromARGB(255, 250, 250, 250),
+                      onPressed: _openGallery,
+                    ),
+                  ),
+                ]))),
     );
   }
 }
