808482b5e8f06b2d4852e55520110095bfdb93e2
ecarlson1 <erikmathewcarlson@gmail.com>
2022-05-12 10:34:37 -0500

    Wrapped image parsing inside class so it can be called from the app

--- lib/parse_dice.dart
+++ lib/parse_dice.dart
@@ -1,21 +1,37 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'dart:io' as io;
-void main() async {
-  var bytes = io.File('realbboggle.jpg').readAsBytesSync();
-  String img64 = base64Encode(bytes);
-  var httpsUri =
-    Uri(scheme: 'https', host: 'api.ocr.space', path: '/parse/image');
-  var payload = {
-    "base64Image": "data:image/jpg;base64, ${img64.toString()}",
-    "OCREngine": "2",
-    "isTable": "true",
-    "scale": "true"};
-  var header = {"apikey": "K83166238588957"};
-  var post = await http.post(httpsUri, body: payload, headers: header);
-  
-  String parsedText = '';
-  var result = jsonDecode(post.body);
-  parsedText = result['ParsedResults'][0]['ParsedText'];
-  print(parsedText);
+
+class DiceParcer{
+  late io.File image;
+  DiceParcer(io.File boardImage) {
+    image = boardImage;
+  }
+
+  Future<List<String>> parse() async {
+    var bytes = image.readAsBytesSync();
+    String img64 = base64Encode(bytes);
+    String ocrResult = await _callOCR(img64);
+    List<String> lines = ocrResult.split('\n');
+    for (String line in lines) {
+      line.replaceAll(' ', '');
+    }
+    return lines;
+  }
+
+  Future<String> _callOCR(img64) async {
+    var httpsUri =
+      Uri(scheme: 'https', host: 'api.ocr.space', path: '/parse/image');
+    var payload = {
+      "base64Image": "data:image/jpg;base64, ${img64.toString()}",
+      "OCREngine": "2",
+      "isTable": "true",
+      "scale": "true"};
+    var header = {"apikey": "K83166238588957"};
+    var post = await http.post(httpsUri, body: payload, headers: header);
+    String parsedText = '';
+    var result = jsonDecode(post.body);
+    parsedText = result['ParsedResults'][0]['ParsedText'];
+    return parsedText;
+  }
 }
